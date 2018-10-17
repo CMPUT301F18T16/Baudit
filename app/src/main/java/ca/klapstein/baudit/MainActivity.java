1652360f56e8309d3b568b8aaae0049cf28f8f5a
Nathan Klapstein <nklapste@ualberta.ca>
2018-10-17 15:35:27 -0600

    Improving Testing Structure (#44)
    * improving `build.gradle` commenting and format
    * Adding comment
    * Adding Test example

--- app/src/main/java/ca/klapstein/baudit/MainActivity.java
+++ app/src/main/java/ca/klapstein/baudit/MainActivity.java
@@ -3,6 +3,10 @@ package ca.klapstein.baudit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+/**
+ * Starting Activity for Baudit.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
