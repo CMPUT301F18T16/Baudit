ad995116eb091adb3a18b80a15d2eae0ce6b471f
nklapste <nklapste@ualberta.ca>
2018-10-17 15:03:26 -0600

    Adding Test example

---
+++ app/src/androidTest/java/ca/klapstein/baudit/MainActivityTest.java
@@ -0,0 +1,25 @@
+package ca.klapstein.baudit;
+
+import android.app.Activity;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example InstrumentedTest for testing MainActivity
+ */
+public class MainActivityTest extends ActivityTestRule<MainActivity> {
+    public MainActivityTest() {
+        super(ca.klapstein.baudit.MainActivity.class);
+    }
+
+    /**
+     * Placeholder test
+     */
+    @Test
+    public void testSuccess() {
+        assertEquals(true,true);
+    }
+}
