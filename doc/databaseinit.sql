a29f711dcba3a6b049ccdfedf085f526efc4545a
Daniel Cones <dcones@ualberta.ca>
2018-10-22 14:37:45 -0600

    Created mark down version of queries, added descriptions to the database initialization tables

--- doc/databaseinit.sql
+++ doc/databaseinit.sql
@@ -1,3 +1,7 @@
+/* 
+This table represents care providers i.e. doctors, nurses, etc.
+Does not have contact information as it is not necessary
+*/
 CREATE TABLE careprovider  (
 id VARCHAR(20) NOT NULL UNIQUE,
 first VARCHAR(50),
@@ -5,6 +9,10 @@ last VARCHAR(50),
 PRIMARY KEY (id)
 );
 
+/*
+This table represents patients.
+Includes contact information.
+*/
 CREATE TABLE patient (
 id VARCHAR(20) NOT NULL UNIQUE,
 first VARCHAR(50),
@@ -14,6 +22,9 @@ phone INT(20),
 PRIMARY KEY (id)
 );
 
+/*
+This table lists all the patients associated with each care provider.
+*/
 CREATE TABLE relationship (
 careprovider VARCHAR(20) NOT NULL,
 patient VARCHAR(20) NOT NULL,
@@ -22,6 +33,12 @@ FOREIGN KEY(careprovider) REFERENCES careprovider(id),
 FOREIGN KEY(patient) REFERENCES patient(id)
 );
 
+/*
+This table holds all the bodyphotos patients upload.
+It is not in the patient table as there are variable
+amounts for each patient, and storing photos separately
+increases performance.
+*/
 CREATE TABLE bodyphoto (
 id INT NOT NULL AUTO_INCREMENT,
 patient VARCHAR(20) NOT NULL,
@@ -30,6 +47,10 @@ PRIMARY KEY (patient, id),
 FOREIGN KEY (patient) REFERENCES patient(id)
 );
 
+/*
+This table represents all of the problems by all of the patients.
+Contains general information about the problem.
+*/
 CREATE TABLE problem (
 id INT NOT NULL AUTO_INCREMENT,
 patient VARCHAR(20) NOT NULL,
@@ -40,6 +61,10 @@ PRIMARY KEY(patient, id),
 FOREIGN KEY(patient) REFERENCES patient(id)
 );
 
+/*
+This table represents all of the records of each problem of each patient.
+Contails the details of the problem at the time the patient inserts the record.
+*/
 CREATE TABLE record (
 id INT NOT NULL AUTO_INCREMENT,
 problem INT NOT NULL,
@@ -57,7 +82,12 @@ FOREIGN KEY(bodyphoto) REFERENCES bodyphoto(id)
 ON DELETE CASCADE
 );
 
-
+/*
+This table holds all the record photos patients upload.
+It is not in the record table as there are variable
+amounts for each record, and storing photos separately
+increases performance.
+*/
 CREATE TABLE photo (
 id  INT NOT NULL AUTO_INCREMENT,
 record INT,
