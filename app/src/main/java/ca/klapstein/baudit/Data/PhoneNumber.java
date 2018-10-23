21bcc116dea8bb55c6edadd80006949ee36a08e0
nklapste <nklapste@ualberta.ca>
2018-10-23 12:14:04 -0600

    Adding default constructors and todos

--- app/src/main/java/ca/klapstein/baudit/Data/PhoneNumber.java
+++ app/src/main/java/ca/klapstein/baudit/Data/PhoneNumber.java
@@ -6,11 +6,17 @@ public class PhoneNumber {
     @NonNull
     private String phoneNumber;
 
+    public PhoneNumber(@NonNull String phoneNumber){
+        // TODO: add validation of phone number string
+        this.phoneNumber = phoneNumber;
+    }
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
+        // TODO: add validation of phone number string
         this.phoneNumber = phoneNumber;
     }
 }
