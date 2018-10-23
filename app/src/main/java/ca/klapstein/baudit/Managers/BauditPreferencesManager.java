50ee325fe56adc446b88da1b9915022296368ac5
nklapste <nklapste@ualberta.ca>
2018-10-23 13:10:27 -0600

    Adding javadoc comment

--- app/src/main/java/ca/klapstein/baudit/Managers/BauditPreferencesManager.java
+++ app/src/main/java/ca/klapstein/baudit/Managers/BauditPreferencesManager.java
@@ -1,5 +1,11 @@
 package ca.klapstein.baudit.Managers;
 
+/**
+ * Helper class for accessing the Android's SharedPreferences for use in Baudit.
+ * <p>
+ * Use {@code Gson} to serialize/deserialize Objects to/from a JSON string for saving/loading
+ * in Android's SharedPreferences.
+ */
 public class BauditPreferencesManager {
     private static final String TAG = "BauditPrefManager";
 }
