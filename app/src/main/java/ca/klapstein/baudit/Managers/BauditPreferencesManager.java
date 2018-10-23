c0002f85cb34754c2475daeec6ef35aa07dc919b
nklapste <nklapste@ualberta.ca>
2018-10-23 13:13:14 -0600

    Adding Gson and extra docs

--- app/src/main/java/ca/klapstein/baudit/Managers/BauditPreferencesManager.java
+++ app/src/main/java/ca/klapstein/baudit/Managers/BauditPreferencesManager.java
@@ -5,6 +5,10 @@ package ca.klapstein.baudit.Managers;
  * <p>
  * Use {@code Gson} to serialize/deserialize Objects to/from a JSON string for saving/loading
  * in Android's SharedPreferences.
+ *
+ * @see com.google.gson.Gson
+ * @see android.content.SharedPreferences
+ * @see android.preference.PreferenceManager
  */
 public class BauditPreferencesManager {
     private static final String TAG = "BauditPrefManager";
