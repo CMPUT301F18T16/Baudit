93987fd107490a0886753cb0308fb4618d0fba97
nklapste <nklapste@ualberta.ca>
2018-10-23 13:31:29 -0600

    adding docs

--- app/src/main/java/ca/klapstein/baudit/Activities/AccountCreateActivity.java
+++ app/src/main/java/ca/klapstein/baudit/Activities/AccountCreateActivity.java
@@ -3,6 +3,13 @@ package ca.klapstein.baudit.Activities;
 import android.support.annotation.Nullable;
 import ca.klapstein.baudit.Data.User;
 
+/**
+ * Activity presenting a account creation page for Baudit.
+ *
+ * After filling out the required forms create a {@code User} and log them into Baudit.
+ *
+ * @see User
+ */
 public class AccountCreateActivity {
     private static final String TAG = "AccountCreateActivity";
 
