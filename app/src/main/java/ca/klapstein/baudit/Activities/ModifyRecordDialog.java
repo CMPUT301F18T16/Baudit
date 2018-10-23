97fba28ccfe2d98a7f1ce423876189a1eb8d3a1e
nklapste <nklapste@ualberta.ca>
2018-10-23 11:52:04 -0600

    Adding extends to a `DialogFragment`

--- app/src/main/java/ca/klapstein/baudit/Activities/ModifyRecordDialog.java
+++ app/src/main/java/ca/klapstein/baudit/Activities/ModifyRecordDialog.java
@@ -1,8 +1,9 @@
 package ca.klapstein.baudit.Activities;
 
+import android.support.v4.app.DialogFragment;
 import ca.klapstein.baudit.Data.Record;
 
-public class ModifyRecordDialog {
+public class ModifyRecordDialog extends DialogFragment {
     private static final String TAG = "ModifyRecordDialog";
 
     private Record record;
--- app/src/main/java/ca/klapstein/baudit/Activities/ViewAccountDialog.java
+++ app/src/main/java/ca/klapstein/baudit/Activities/ViewAccountDialog.java
@@ -1,8 +1,9 @@
 package ca.klapstein.baudit.Activities;
 
+import android.support.v4.app.DialogFragment;
 import ca.klapstein.baudit.Data.User;
 
-public class ViewAccountDialog {
+public class ViewAccountDialog extends DialogFragment {
     private static final String TAG = "ViewAccountDialog";
 
     private User user;
