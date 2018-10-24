e32cad0c2d248ad7ce659c65d1ad908de350b8d0
nklapste <nklapste@ualberta.ca>
2018-10-24 16:24:18 -0600

    Adding test stubs

---
+++ app/src/test/java/ca/klapstein/baudit/Data/UsernameTest.java
@@ -0,0 +1,16 @@
+package ca.klapstein.baudit.Data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UsernameTest {
+
+    @Test
+    public void getUsername() {
+    }
+
+    @Test
+    public void setUsername() {
+    }
+}
