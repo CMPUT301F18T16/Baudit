fce80b425c2c506b56ea464ff8e9ae36f3269e73
nklapste <nklapste@ualberta.ca>
2018-10-24 16:24:18 -0600

    Adding test stubs

---
+++ app/src/test/java/ca/klapstein/baudit/Data/PatientTest.java
@@ -0,0 +1,36 @@
+package ca.klapstein.baudit.Data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PatientTest {
+
+    @Test
+    public void getProblemTreeSet() {
+    }
+
+    @Test
+    public void getUsername() {
+    }
+
+    @Test
+    public void setUsername() {
+    }
+
+    @Test
+    public void getContactInfo() {
+    }
+
+    @Test
+    public void setContactInfo() {
+    }
+
+    @Test
+    public void getPassword() {
+    }
+
+    @Test
+    public void setPassword() {
+    }
+}
