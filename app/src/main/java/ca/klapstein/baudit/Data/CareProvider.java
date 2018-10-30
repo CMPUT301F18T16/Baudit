3f43fcbdc619984574fc594032c2fe356b9f0734
jalqueza <jerwynalqueza@gmail.com>
2018-10-30 14:36:38 -0600

    careprovider and patient update

--- app/src/main/java/ca/klapstein/baudit/Data/CareProvider.java
+++ app/src/main/java/ca/klapstein/baudit/Data/CareProvider.java
@@ -1,5 +1,7 @@
 package ca.klapstein.baudit.Data;
 
+import java.util.Iterator;
+
 /**
  * Class that represents a Care Provider.
  */
@@ -7,4 +9,72 @@ public class CareProvider extends User {
     private static final String TAG = "CareProvider";
 
     private PatientTreeSet assignedPatientTreeSet;
+
+    // UC-01.02.01: list all medical problems for a related account
+    public ProblemTreeSet getProblemTreeSet(Patient patient){ return patient.getProblemTreeSet(); }
+
+    // UC-02.01.01:  list all the records for a related medical problem
+    public RecordTreeSet getRecordTreeSet(Patient patient, Problem problem){
+        ProblemTreeSet problemTreeSet = patient.getProblemTreeSet();
+
+        RecordTreeSet recordTreeSet = new RecordTreeSet();
+
+        Iterator itr = problemTreeSet.iterator();
+        while(itr.hasNext()){
+            // TODO: if problem matches get the records
+        }
+
+        return recordTreeSet;
+    }
+
+    // UC-03.02.01: edit account contact information
+    public void editContactInfo(ContactInfo contactInfo){ this.setContactInfo(contactInfo);}
+
+    // UC-03.03.01: view username or account contact information
+    public ContactInfo getContactInfo(){ return this.getContactInfo(); }
+    public Username getUsername(){ return this.getUsername(); }
+
+    // UC-06.01.01: list patients assigned to care providers
+    public PatientTreeSet getAssignedPatientTreeSet(){ return assignedPatientTreeSet; }
+
+    // UC-07-01.01: assign patient to care providers
+    public void assignPatient(Patient patient){ assignedPatientTreeSet.add(patient); }
+
+    // UC-07.02.01: list all patients TODO: not sure how to
+
+    // UC-07.03.01: add comment record to medical problem
+    public void addCommentRecord(Record record, Problem problem){
+        RecordTreeSet recordTreeSet = problem.getRecordTreeSet();
+        recordTreeSet.add(record);
+    }
+
+    // UC-03.03.01: login into account
+    public void attemptLogin(Username username, Password password){
+        if (this.getUsername().equals(username) && this.getPassword().equals(password));
+        // TODO: successful
+    }
+
+    // UC-03.04.01: logout of account TODO
+
+    // UC-04.01.01: search for problem or record
+    public ProblemTreeSet searchProblem(String searchString){
+        ProblemTreeSet matchingProblems = new ProblemTreeSet();
+
+        Iterator itr = assignedPatientTreeSet.iterator();
+        while(itr.hasNext()){
+            // TODO: iterate patients the problems, search using regex and add to matching problems
+        }
+
+        return matchingProblems;
+    }
+
+    public RecordTreeSet searchRecords(String searchString){
+
+        RecordTreeSet matchingRecords = new RecordTreeSet();
+
+        // TODO: iterate problems then records, search using regex and add to matching problems
+
+        return matchingRecords;
+    }
 }
+
