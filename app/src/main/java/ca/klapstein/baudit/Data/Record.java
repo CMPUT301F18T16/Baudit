2ace8a0a06cb8968d5ddf232ee63b1215c00682a
wlt30 <wlt@ualberta.ca>
2018-10-31 16:55:21 -0600

    Moved common functions in CareProvider and Patient to User Class. Edited Record attributes and addRecord function in user

--- app/src/main/java/ca/klapstein/baudit/Data/Record.java
+++ app/src/main/java/ca/klapstein/baudit/Data/Record.java
@@ -1,5 +1,8 @@
 package ca.klapstein.baudit.Data;
 
+import java.util.Collection;
+import java.util.Date;
+
 /**
  * Data class representing a Record for a Medical Problem {@code Problem}.
  *
@@ -7,4 +10,8 @@ package ca.klapstein.baudit.Data;
  */
 public class Record {
     private static final String TAG = "Record";
+    public Username username;
+    public String timestamp;
+    public Collection<RecordPhoto> recordPhotos; //not approved yet
+    public Collection<BodyPhoto> bodyPhotos; // not approved yet
 }
--- app/src/main/java/ca/klapstein/baudit/Data/User.java
+++ app/src/main/java/ca/klapstein/baudit/Data/User.java
@@ -2,6 +2,12 @@ package ca.klapstein.baudit.Data;
 
 import android.support.annotation.NonNull;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.sql.Timestamp;
+import java.util.TimeZone;
+
 /**
  * Abstract class representing a basic account/user of Baudit.
  */
@@ -42,5 +48,34 @@ public abstract class User {
 
     public void setPassword(@NonNull Password password) {
         this.password = password;
+    }
+
+    // UC-03.03.01: login into account
+    public boolean attemptLogin(Username username, Password password){
+        if (this.getUsername().getUsername().equals(username.getUsername())) {
+            if (this.getPassword().getPassword().equals(password.getPassword()))
+                return true;
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+
+    // UC-07.03.01: add comment record to medical problem
+    public void addCommentRecord(Record record, Problem problem){
+        RecordTreeSet recordTreeSet = problem.getRecordTreeSet();
+        DateFormat date  = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+        String timestamp = date.format(new Date());
+        //
+        recordTreeSet.add(record);
+        record.username = this.username;
+        record.timestamp = timestamp;
+    }
+
+    // UC-03.04.01: logout of account TODO
+    public void logout() {
+        // call LogoutDialog activity
+        // go back to MAIN
     }
 }
