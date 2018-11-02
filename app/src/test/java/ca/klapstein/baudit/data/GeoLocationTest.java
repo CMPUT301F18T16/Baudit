5b1aeecbf01bb29a422df2abc1aa3b0679d4088d
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-02 13:14:24 -0600

    adding simple `GeoLocation` tests

--- app/src/test/java/ca/klapstein/baudit/data/GeoLocationTest.java
+++ app/src/test/java/ca/klapstein/baudit/data/GeoLocationTest.java
@@ -4,30 +4,47 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class GeoLocationTest {
+
+    private static final double valid_lat = 10.00001;
+    private static final double valid_lon = 10.00001;
 
     @Before
     public void setUp() {
+
     }
 
     @After
     public void tearDown() {
+
     }
 
     @Test
     public void getLon() {
+        GeoLocation geoLocation = new GeoLocation(valid_lat, valid_lon);
+        assertEquals(10.00001, geoLocation.getLon());
     }
 
     @Test
     public void setLon() {
+        GeoLocation geoLocation = new GeoLocation(valid_lat, valid_lon);
+        geoLocation.setLon(5.00001);
+        assertEquals(5.00001, geoLocation.getLon());
     }
 
     @Test
     public void getLat() {
+        GeoLocation geoLocation = new GeoLocation(valid_lat, valid_lon);
+        assertEquals(10.00001, geoLocation.getLat());
     }
 
     @Test
     public void setLat() {
+        GeoLocation geoLocation = new GeoLocation(valid_lat, valid_lon);
+        geoLocation.setLat(5.00001);
+        assertEquals(5.00001, geoLocation.getLat());
     }
 
     @Test
