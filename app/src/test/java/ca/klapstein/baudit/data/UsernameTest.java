fa7e1a264f8d5e02fa97be49486eac9ce65e744e
Thomas Lorincz <tlorincz@ualberta.ca>
2018-11-02 14:12:27 -0600

    Improved Username unit tests

--- app/src/test/java/ca/klapstein/baudit/data/UsernameTest.java
+++ app/src/test/java/ca/klapstein/baudit/data/UsernameTest.java
@@ -1,10 +1,7 @@
 package ca.klapstein.baudit.data;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -18,62 +15,34 @@ public class UsernameTest {
     }
 
     @Test
-    public void testGetUsername() {
-        Username username = null;
-
-        try {
-            username = new Username("testUser");
-        } catch(IllegalArgumentException e) {
-            fail();
-        }
-
-        assertNotNull(username);
-        assertEquals("testUser", username.getUsername());
-    }
-
-    @Test
     public void testSetUsername() {
-        try {
-            Username username = new Username("testUser");
-            assertEquals("testUser", username.getUsername());
-            username.setUsername("testUser1");
-            assertEquals("testUser1", username.getUsername());
-            username.setUsername("1234567890");
-            assertEquals("1234567890", username.getUsername());
-            username.setUsername("123test123");
-            assertEquals("123test123", username.getUsername());
-        } catch (IllegalArgumentException e) {
-            fail();
-        }
-
+        Username username = new Username("testUser");
+        assertEquals("testUser", username.getUsername());
+        username.setUsername("testUser1");
+        assertEquals("testUser1", username.getUsername());
+        username.setUsername("1234567890");
+        assertEquals("1234567890", username.getUsername());
+        username.setUsername("123test123");
+        assertEquals("123test123", username.getUsername());
     }
 
-    @Test
-    public void testSetUsernameInvalid() {
-        boolean success = false;
-        try {
-            new Username("test");
-            success = true;
-        } catch (IllegalArgumentException e) {}
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetUsernameInvalid1() {
+        new Username("test");
+    }
 
-        try {
-            new Username("testUserThatIsWayTooLong");
-            success = true;
-        } catch (IllegalArgumentException e) {}
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetUsernameInvalid2() {
+        new Username("testUserThatIsWayTooLong");
+    }
 
-        try {
-            new Username("Ťéŝţ");
-            success = true;
-        } catch (IllegalArgumentException e) {}
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetUsernameInvalid3() {
+        new Username("Ťéŝţ");
+    }
 
-        try {
-            new Username("");
-            success = true;
-        } catch (IllegalArgumentException e) {}
-
-
-        if (success) {
-            fail();
-        }
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetUsernameInvalid4() {
+        new Username("");
     }
 }
