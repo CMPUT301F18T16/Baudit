609cf44f7e62b7cb80e1720917433ab3d740e9ce
DeveloperJeffb <33044463+DeveloperJeffb@users.noreply.github.com>
2018-11-02 01:57:41 -0600

    Added comparable function to problem  & Finished unit testing for ProblemTreeSetTest.java

--- app/src/test/java/ca/klapstein/baudit/data/ProblemTreeSetTest.java
+++ app/src/test/java/ca/klapstein/baudit/data/ProblemTreeSetTest.java
@@ -3,6 +3,9 @@ package ca.klapstein.baudit.data;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 
 public class ProblemTreeSetTest {
 
@@ -20,6 +23,26 @@ public class ProblemTreeSetTest {
 
     @Test
     public void testStub() {
+        problemTreeSet = new ProblemTreeSet();
+
+        Problem problem1 = new Problem("title1", "1");
+        Problem problem2 = new Problem("title2", "2");
+        Problem problem3 = new Problem("title3", "3");
+        Problem problem4 = new Problem("title4", "4");
+        assertTrue(problemTreeSet.add(problem1));
+        assertTrue(problemTreeSet.add(problem2));
+
+        assertTrue(problemTreeSet.contains(problem2));
+
+        assertTrue(problemTreeSet.remove(problem2));
+
+        assertFalse(problemTreeSet.contains(problem2));
+        assertTrue(problemTreeSet.contains(problem1));
+
+        assertTrue(problemTreeSet.add(problem3));
+        assertTrue(problemTreeSet.add(problem4));
+
+
         // TODO: write tests
     }
 }
