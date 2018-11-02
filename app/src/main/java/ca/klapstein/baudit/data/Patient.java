5d41d3de637675fdfd0ce3c2726faf9d84c82df6
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-02 16:51:26 -0600

    adding javadoc

--- app/src/main/java/ca/klapstein/baudit/data/Patient.java
+++ app/src/main/java/ca/klapstein/baudit/data/Patient.java
@@ -6,6 +6,10 @@ package ca.klapstein.baudit.data;
 public class Patient extends User {
     private static final String TAG = "Patient";
     private ProblemTreeSet problemTreeSet;
+
+    /**
+     * A {@code Patient} can only have one {@code BodyPhoto}.
+     */
     private BodyPhoto bodyPhoto;
 
     public ProblemTreeSet getProblemTreeSet() {
@@ -16,6 +20,11 @@ public class Patient extends User {
         return bodyPhoto;
     }
 
+    /**
+     * Setter for a {@code Patient}'s {@code BodyPhoto}.
+     *
+     * @param bodyPhoto {@code BodyPhoto}
+     */
     public void setBodyPhoto(BodyPhoto bodyPhoto) {
         this.bodyPhoto = bodyPhoto;
     }
