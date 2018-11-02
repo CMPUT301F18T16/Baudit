98ac8e7f9e033c3878820b29a89d6246722de833
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-02 16:32:37 -0600

    adding additional fields to `Problem`

--- app/src/main/java/ca/klapstein/baudit/data/Problem.java
+++ app/src/main/java/ca/klapstein/baudit/data/Problem.java
@@ -1,5 +1,7 @@
 package ca.klapstein.baudit.data;
 
+import java.util.Date;
+
 /**
  * Data class representing a Medical Problem for a {@code Patient}.
  *
@@ -8,8 +10,29 @@ package ca.klapstein.baudit.data;
 public class Problem {
     private static final String TAG = "Problem";
     private RecordTreeSet recordTreeSet;
+    private String title;
+    private String description;
+    private Date dateStarted;
 
     public RecordTreeSet getRecordTreeSet() {
         return recordTreeSet;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        // TODO: add validator
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        // TODO: add validator
+        this.title = title;
     }
 }
