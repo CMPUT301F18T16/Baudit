5a64cf793d2a7b5e31e2fc31ebd0f4a5e643a9d8
Daniel Cones <dcones@ualberta.ca>
2018-11-01 23:19:07 -0600

    Parameterized and try/catch removal

--- app/src/test/java/ca/klapstein/baudit/data/PasswordTest.java
+++ app/src/test/java/ca/klapstein/baudit/data/PasswordTest.java
@@ -1,8 +1,10 @@
 package ca.klapstein.baudit.data;
 
-import org.junit.After;
-import org.junit.Before;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertEquals;
@@ -10,76 +12,77 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(Parameterized.class)
 public class PasswordTest {
+
+    private String input;
+
+    public PasswordTest(String input) {
+        this.input = input;
+    }
+
+    @Parameters
+    public static String[] data1() {
+        return new String[] { "abcd1234", "password", "011235813"};
+    }
+
     @Test
     public void test_validPassword() {
-        assertTrue(Password.isValid("abcd1234"));
-        assertTrue(Password.isValid("password"));
-        assertTrue(Password.isValid("011235813"));
+        assertTrue(Password.isValid(input));
+    }
+
+    @Parameters
+    public static String[] data2() {
+        return new String[] { "password!", "short", "thisPasswordIsWayTooExcessive"};
     }
 
     @Test
     public void test_invalidPassword() {
-        assertFalse(Password.isValid("password!"));
-        assertFalse(Password.isValid("short"));
-        assertFalse(Password.isValid("thisPasswordIsWayTooExcessive"));
+        assertFalse(Password.isValid(input));
+    }
+
+    @Parameters
+    public static String[] data3() {
+        return new String[] { "passwordConstruct"};
     }
 
     @Test
     public void testPasswordConstructor() {
-        Password password = new Password("passwordConstruct");
+        Password password = new Password(input);
         assertNotNull(password);
-        assertEquals("passwordConstruct", password.getPassword());
+        assertEquals(input, password.getPassword());
+    }
+
+    @Parameters
+    public static String[] data4() {
+        return new String[] { "passwordGet" };
     }
 
     @Test
     public void testGetPassword() {
-        Password password = null;
-
-        try {
-            password = new Password("passwordGet");
-        } catch(IllegalArgumentException e) {
-            fail();
-        }
-
+        Password password = new Password(input);
         assertNotNull(password);
-        assertEquals("passwordGet", password.getPassword());
+        assertEquals(input, password.getPassword());
+    }
+
+    @Parameters
+    public static String[] data5() {
+        return new String[] { "abcd1234", "password", "011235813"};
     }
 
     @Test
     public void testSetPasswordValid() {
-        try {
-            Password password = new Password("abcd1234");
-            assertEquals("abcd1234", password.getPassword());
-            password.setPassword("password");
-            assertEquals("password", password.getPassword());
-            password.setPassword("011235813");
-            assertEquals("011235813", password.getPassword());
-        } catch (IllegalArgumentException e) {
-            fail();
-        }
+        Password password = new Password(input);
+        assertEquals(input, password.getPassword());
+    }
+
+    @Parameters
+    public static String[] data6() {
+        return new String[] { "@bad!password", "", "thisPasswordIsWayTooExcessive"};
     }
 
     @Test
     public void testSetPasswordInvalid() {
-        boolean success = false;
-        try {
-            new Password("@bad!password");
-            success = true;
-        } catch (IllegalArgumentException e) {}
-
-        try {
-            new Password("");
-            success = true;
-        } catch (IllegalArgumentException e) {}
-
-        try {
-            new Password("thisPasswordIsWayTooExcessive");
-            success = true;
-        } catch (IllegalArgumentException e) {}
-
-        if (success) {
-            fail();
-        }
+        new Password(input);
     }
 }
