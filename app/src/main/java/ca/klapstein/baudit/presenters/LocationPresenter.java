a77357d14c9401985a02d03522a46ee7aea9f54f
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-02 12:18:25 -0600

    adding `GeoLocation` class

--- app/src/main/java/ca/klapstein/baudit/presenters/LocationPresenter.java
+++ app/src/main/java/ca/klapstein/baudit/presenters/LocationPresenter.java
@@ -1,5 +1,6 @@
 package ca.klapstein.baudit.presenters;
 
+import ca.klapstein.baudit.data.GeoLocation;
 import ca.klapstein.baudit.views.LocationView;
 
 /**
@@ -10,8 +11,13 @@ import ca.klapstein.baudit.views.LocationView;
 public class LocationPresenter extends Presenter<LocationView> {
     private static final String TAG = "LocationPresenter";
 
+    public GeoLocation location;
 
     public LocationPresenter(LocationView view) {
         super(view);
+    }
+
+    public boolean validateLocation(long posx, long posy) {
+        return true;
     }
 }
