d14c80f4a4ac92e926dbc665d0d8bc0f4daf5762
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-02 08:56:28 -0600

    adding abstracting data management class between local and remote

---
+++ app/src/main/java/ca/klapstein/baudit/managers/BauditDataManager.java
@@ -0,0 +1,11 @@
+package ca.klapstein.baudit.managers;
+
+/**
+ * Helper class that interacts with the {@code BauditDataBaseManager} (local) and {@code BauditRemoteManager} (remote)
+ * to keep MVP presenters blind to the fact data may be coming from either a remote or local source.
+ *
+ * @see BauditDataBaseManager
+ * @see BauditRemoteManager
+ */
+public class BauditDataManager {
+}
