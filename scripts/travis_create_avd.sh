291c1fc73dacf97ebbe7416075bb94a82d5c42b2
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-06 11:49:55 -0700

    attempting to use env vars

--- scripts/travis_create_avd.sh
+++ scripts/travis_create_avd.sh
@@ -3,9 +3,8 @@
 # Creates and starts an emulator.
 
 # the android-16 emulator is very quick for some reason
-
-android-update-sdk --components=sys-img-armeabi-v7a-android-16 --accept-licenses='android-sdk-license-[0-9a-f]{8}'
-echo no | android create avd --force -n test -t android-16 --abi armeabi-v7a --skin QVGA
+android-update-sdk --components=sys-img-$ANDROID_ABI-android-$ANDROID_EMULATOR_LEVEL --accept-licenses='android-sdk-license-[0-9a-f]{8}'
+echo "no" | android create avd --force -n test -t "android-"$ANDROID_EMULATOR_LEVEL --abi $ANDROID_ABI --skin QVGA
 emulator -avd test -no-audio -no-skin -netfast -no-window &
 
 exit 0
