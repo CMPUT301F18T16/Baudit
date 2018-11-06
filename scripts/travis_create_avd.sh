9193dcb80267ec418ce8ca273ebe80af21725f81
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-06 11:52:33 -0700

    simplifying ci

--- scripts/travis_create_avd.sh
+++ scripts/travis_create_avd.sh
@@ -4,7 +4,7 @@
 
 # the android-16 emulator is very quick for some reason
 android-update-sdk --components=sys-img-$ANDROID_ABI-android-$ANDROID_EMULATOR_LEVEL --accept-licenses='android-sdk-license-[0-9a-f]{8}'
-echo "no" | android create avd --force -n test -t "android-"$ANDROID_EMULATOR_LEVEL --abi $ANDROID_ABI --skin QVGA
+echo no | android create avd --force -n test -t "android-"$ANDROID_EMULATOR_LEVEL --abi $ANDROID_ABI --skin QVGA
 emulator -avd test -no-audio -no-skin -netfast -no-window &
 
 exit 0
