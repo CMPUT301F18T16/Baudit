7238b471bb4527e6da28c22cca840617a19d3a72
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-06 10:37:51 -0700

    cleaning docs

--- scripts/travis_create_avd.sh
+++ scripts/travis_create_avd.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 # Creates and starts an emulator.
-# Avoid the tempation to skip the boot animation. CI scripts use that to figure out when the device is ready.
-# The emulator is started in the background. This is to avoid holding up the build until we need it.
 
 android-update-sdk --components=sys-img-armeabi-v7a-android-16 --accept-licenses='android-sdk-license-[0-9a-f]{8}'
 echo no | android create avd --force -n test -t android-16 --abi armeabi-v7a --skin QVGA
