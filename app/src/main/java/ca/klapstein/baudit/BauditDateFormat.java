4a1279eca1129358d576eba880602d9f58c8f245
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-05 22:58:53 -0700

    adding javadocs denoting the `BauditDateFormat` date format string

--- app/src/main/java/ca/klapstein/baudit/BauditDateFormat.java
+++ app/src/main/java/ca/klapstein/baudit/BauditDateFormat.java
@@ -5,6 +5,8 @@ import java.util.Locale;
 
 /**
  * Helper class that provides the default {@code SimpleDateFormat} for Baudit.
+ *
+ * Dates are formatted to a {@code "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"} format.
  */
 public class BauditDateFormat {
     static public SimpleDateFormat getBauditDateFormat() {
