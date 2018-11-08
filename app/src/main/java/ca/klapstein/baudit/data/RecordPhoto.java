60af052a72c2b00096cc90af87f3cc579bbf7934
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-08 09:04:17 -0700

    adding base skeleton for `RecordPhoto`

--- app/src/main/java/ca/klapstein/baudit/data/RecordPhoto.java
+++ app/src/main/java/ca/klapstein/baudit/data/RecordPhoto.java
@@ -5,13 +5,34 @@ import android.graphics.Bitmap;
 public class RecordPhoto implements Photo {
     private static final String TAG = "RecordPhoto";
 
-    @Override
-    public Bitmap getBitmap() {
-        return null;
+    private Bitmap bitmap;
+
+    public RecordPhoto(Bitmap bitmap) throws IllegalArgumentException {
+        this.setBitmap(bitmap);
+    }
+
+    /**
+     * Check if a given {@code Bitmap} image is valid for usage as a {@code RecordPhoto}.
+     *
+     * @param bitmap {@code Bitmap} the Bitmap image to validate
+     * @return {@code boolean} {@code true} if the {@code Bitmap} is valid, otherwise {@code false}
+     */
+    static public boolean isValid(Bitmap bitmap) {
+        // TODO: implement
+        return true;
     }
 
     @Override
-    public void setBitmap(Bitmap bitmap) {
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
 
+    @Override
+    public void setBitmap(Bitmap bitmap) throws IllegalArgumentException {
+        if (!isValid(bitmap)) {
+            throw new IllegalArgumentException("invalid bitmap for RecordPhoto");
+        } else {
+            this.bitmap = bitmap;
+        }
     }
 }
