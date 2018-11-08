710b8bf13181333992108060703d8d23ea59e9ee
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-08 10:05:31 -0700

    Improving class structure for `Account` related `View`s

--- app/src/main/java/ca/klapstein/baudit/presenters/AccountPresenter.java
+++ app/src/main/java/ca/klapstein/baudit/presenters/AccountPresenter.java
@@ -1,12 +1,12 @@
 package ca.klapstein.baudit.presenters;
 
 import ca.klapstein.baudit.data.Account;
-import ca.klapstein.baudit.views.View;
+import ca.klapstein.baudit.views.AccountView;
 
 /**
  * MVP presenter for a {@code Account}.
  */
-abstract public class AccountPresenter<V extends View> extends Presenter<V> {
+abstract public class AccountPresenter<V extends AccountView> extends Presenter<V> {
     private static final String TAG = "AccountPresenter";
 
     protected Account account;
