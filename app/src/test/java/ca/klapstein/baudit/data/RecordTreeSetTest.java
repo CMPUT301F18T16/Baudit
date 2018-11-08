e1fd27f9d293ec0242964bc3b10c809a04ae2f48
DeveloperJeffb <33044463+DeveloperJeffb@users.noreply.github.com>
2018-11-08 10:42:11 -0700

    fixing build issues

--- app/src/test/java/ca/klapstein/baudit/data/RecordTreeSetTest.java
+++ app/src/test/java/ca/klapstein/baudit/data/RecordTreeSetTest.java
@@ -4,8 +4,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.TreeSet;
-
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
