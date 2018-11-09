b0fd07ef6d91a72b6be33864177e0eac4a036ac2
Thomas Lorincz <tlorincz@ualberta.ca>
2018-11-09 15:19:53 -0700

    Added instrumentation tests for LoginCareProviderActivity

--- app/src/androidTest/java/ca/klapstein/baudit/Activities/LoginCareProviderActivityTest.java
+++ app/src/androidTest/java/ca/klapstein/baudit/Activities/LoginCareProviderActivityTest.java
@@ -2,10 +2,14 @@ package ca.klapstein.baudit.activities;
 
 import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
+import android.widget.EditText;
+
 import com.robotium.solo.Solo;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import ca.klapstein.baudit.R;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 
@@ -25,22 +29,61 @@ public class LoginCareProviderActivityTest extends ActivityTestRule<LoginCarePro
 
     @After
     public void tearDown() {
+        solo = null;
     }
 
+    /**
+     * Ensures that the activity is created properly.
+     */
     @Test
-    public void onCreate() {
+    public void testOnCreate() {
+        solo.waitForActivity(LoginCareProviderActivity.class, 2000);
         solo.assertCurrentActivity("Wrong Activity", LoginCareProviderActivity.class);
     }
 
+    /**
+     * Tests a valid login. Should open the PatientListActivity.
+     * Will be updated when the remote login validation is completed.
+     */
     @Test
     public void testLoginSuccess() {
+        solo.enterText((EditText) solo.getView(R.id.enter_care_provider_username), "test");
+        solo.enterText((EditText) solo.getView(R.id.enter_care_provider_password), "foo");
+        solo.clickOnView(solo.getView(R.id.login_care_provider_button));
+        solo.waitForActivity(PatientListActivity.class, 2000);
+        solo.assertCurrentActivity("Wrong Activity", PatientListActivity.class);
     }
 
+    /**
+     * Tests an invalid login attempt. Should not open a new activity.
+     * Will be updated when the remote login validation is completed.
+     */
     @Test
     public void testLoginFail() {
+        solo.enterText((EditText) solo.getView(R.id.enter_care_provider_username), "wrong");
+        solo.enterText((EditText) solo.getView(R.id.enter_care_provider_password), "wrong");
+        solo.clickOnView(solo.getView(R.id.login_care_provider_button));
+        solo.waitForActivity(LoginCareProviderActivity.class, 2000);
+        solo.assertCurrentActivity("Wrong Activity", LoginCareProviderActivity.class);
     }
 
+    /**
+     * Tests clicking the sign up button. Should open the CreateCareProviderAccountActivity.
+     */
     @Test
-    public void onLogin() {
+    public void testRegister() {
+        solo.clickOnView(solo.getView(R.id.register_care_provider_button));
+        solo.waitForActivity(CreateCareProviderAccountActivity.class, 2000);
+        solo.assertCurrentActivity("Wrong Activity", CreateCareProviderAccountActivity.class);
+    }
+
+    /**
+     * Tests switching to the LoginPatientActivity.
+     */
+    @Test
+    public void testSwitchToPatientLogin() {
+        solo.clickOnView(solo.getView(R.id.log_in_as_patient_button));
+        solo.waitForActivity(LoginPatientActivity.class, 2000);
+        solo.assertCurrentActivity("Wrong Activity", LoginPatientActivity.class);
     }
 }
