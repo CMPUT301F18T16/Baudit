09bd06fff37c23f012e21a447ac89f54bbd8b99a
Thomas Lorincz <tlorincz@ualberta.ca>
2018-11-10 23:07:06 -0700

    Another attempt 🤠

--- scripts/travis_create_avd.sh
+++ scripts/travis_create_avd.sh
@@ -2,7 +2,7 @@
 
 # Creates and starts an android emulator.
 
-echo no | android create avd --force -n test -t android-28 --abi google_apis/armeabi-v7a
+echo no | android create avd --force -n test -t android-28 --abi armeabi-v7a
 emulator -avd test -no-audio -no-skin -netfast -no-window &
 
 exit 0
