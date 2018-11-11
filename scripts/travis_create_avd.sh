50adbfcd825f53f2020c90224b63d92c24b7e7bd
Thomas Lorincz <tlorincz@ualberta.ca>
2018-11-10 23:32:25 -0700

    Reverted back to 11ff06a

--- scripts/travis_create_avd.sh
+++ scripts/travis_create_avd.sh
@@ -2,7 +2,10 @@
 
 # Creates and starts an android emulator.
 
-echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
+# the android-16 emulator is very quick for some reason
+
+android-update-sdk --components=sys-img-armeabi-v7a-android-16 --accept-licenses='android-sdk-license-[0-9a-f]{8}'
+echo no | android create avd --force -n test -t android-16 --abi armeabi-v7a --skin QVGA
 emulator -avd test -no-audio -no-skin -netfast -no-window &
 
 exit 0
