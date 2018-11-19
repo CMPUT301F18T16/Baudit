cff0474f447739ef8ed0e6b997ae6beb6daec53d
dcones <dcones@ualberta.ca>
2018-11-18 17:59:21 -0700

    Enabled UsernameTest, failing due to 'Method execute in android.os.AsyncTask not mocked'

--- app/src/test/java/ca/klapstein/baudit/data/UsernameTest.java
+++ app/src/test/java/ca/klapstein/baudit/data/UsernameTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -11,40 +12,40 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(Enclosed.class)
 public class UsernameTest {
 // TODO: enable when a RemoteModel is implemented or mocked
-//    @RunWith(Parameterized.class)
-//    public static class ValidUserNameTest {
-//        @Parameterized.Parameters(name = "{index}: valid({0})={1}")
-//        public static String[] validUserName() {
-//            return new String[]{"testUser", "testUser1", "@!516!@4!@$!2°▐"};
-//        }
-//
-//        private String input;
-//
-//        public ValidUserNameTest(String input) {
-//            this.input = input;
-//        }
-//
-//        @Test
-//        public void testUsernameConstructor() {
-//            Username username = new Username(input);
-//            assertNotNull(username);
-//            assertEquals(input, username.getUsernameString());
-//        }
-//
-//        @Test
-//        public void testGetUsername() {
-//            Username username = new Username(input);
-//            assertNotNull(username);
-//            assertEquals(input, username.getUsernameString());
-//        }
-//
-//        @Test
-//        public void testSetUsername() {
-//            Username username = new Username(input);
-//            username.setUsernameString(input);
-//            assertEquals(input, username.getUsernameString());
-//        }
-//    }
+    @RunWith(Parameterized.class)
+    public static class ValidUserNameTest {
+        @Parameters(name = "{index}: valid({0})={1}")
+        public static String[] validUserName() {
+            return new String[]{"testUser", "testUser1", "@!516!@4!@$!2°▐"};
+        }
+
+        private String input;
+
+        public ValidUserNameTest(String input) {
+            this.input = input;
+        }
+
+        @Test
+        public void testUsernameConstructor() {
+            Username username = new Username(input);
+            assertNotNull(username);
+            assertEquals(input, username.getUsernameString());
+        }
+
+        @Test
+        public void testGetUsername() {
+            Username username = new Username(input);
+            assertNotNull(username);
+            assertEquals(input, username.getUsernameString());
+        }
+
+        @Test
+        public void testSetUsername() {
+            Username username = new Username(input);
+            username.setUsernameString(input);
+            assertEquals(input, username.getUsernameString());
+        }
+    }
 
     @RunWith(Parameterized.class)
     public static class InvalidUserNameTest {
@@ -54,7 +55,7 @@ public class UsernameTest {
             this.input = input;
         }
 
-        @Parameterized.Parameters(name = "{index}: invalid({0})={1}")
+        @Parameters(name = "{index}: invalid({0})={1}")
         public static String[] invalidUserName() {
             return new String[]{"short", "testUserThatIsWayTooLong", ""};
         }
