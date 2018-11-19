666b040bbd4a57288b2884cd5f5fa10dbf32eeda
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-19 13:51:02 -0700

    fixing login success tests

--- app/src/androidTest/java/ca/klapstein/baudit/activities/LoginPatientActivityTest.java
+++ app/src/androidTest/java/ca/klapstein/baudit/activities/LoginPatientActivityTest.java
@@ -3,14 +3,11 @@ package ca.klapstein.baudit.activities;
 import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import android.widget.EditText;
-
+import ca.klapstein.baudit.R;
 import com.robotium.solo.Solo;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import ca.klapstein.baudit.R;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 
@@ -47,8 +44,9 @@ public class LoginPatientActivityTest extends ActivityTestRule<LoginPatientActiv
      */
     @Test
     public void testLoginSuccess() {
-        solo.enterText((EditText) solo.getView(R.id.enter_patient_username), "test");
-        solo.enterText((EditText) solo.getView(R.id.enter_patient_password), "foo");
+        // TODO: add hook to ensure test patient is added to the remote or make some mock
+        solo.enterText((EditText) solo.getView(R.id.enter_patient_username), "TESTPatient1");
+        solo.enterText((EditText) solo.getView(R.id.enter_patient_password), "foobar123");
         solo.clickOnView(solo.getView(R.id.login_patient_button));
         solo.assertCurrentActivity("Wrong Activity", PatientHomeActivity.class);
     }
