aba025e833cfc369aa79e03f109b39fa4b32daf9
DeveloperJeffb <33044463+DeveloperJeffb@users.noreply.github.com>
2018-11-20 13:30:49 -0700

    finished basic functionality of create_care_provider_account

---
+++ app/src/main/java/ca/klapstein/baudit/activities/CareProviderHomeActivity.java
@@ -0,0 +1,16 @@
+package ca.klapstein.baudit.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ca.klapstein.baudit.R;
+
+public class CareProviderHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_care_provider_home);
+    }
+
+}
