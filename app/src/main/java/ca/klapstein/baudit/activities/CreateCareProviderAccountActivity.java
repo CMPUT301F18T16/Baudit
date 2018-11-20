f83a01b38f13d3f1b863317750145f945e2fe2b9
DeveloperJeffb <33044463+DeveloperJeffb@users.noreply.github.com>
2018-11-20 14:58:03 -0700

    activity

--- app/src/main/java/ca/klapstein/baudit/activities/CreateCareProviderAccountActivity.java
+++ app/src/main/java/ca/klapstein/baudit/activities/CreateCareProviderAccountActivity.java
@@ -1,7 +1,13 @@
 package ca.klapstein.baudit.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import ca.klapstein.baudit.R;
 import ca.klapstein.baudit.presenters.CreateCareProviderAccountPresenter;
 import ca.klapstein.baudit.views.CreateCareProviderAccountView;
@@ -13,9 +19,25 @@ import ca.klapstein.baudit.views.CreateCareProviderAccountView;
  * @see ca.klapstein.baudit.data.CareProvider
  */
 public class CreateCareProviderAccountActivity extends AppCompatActivity
-    implements CreateCareProviderAccountView {
+        implements CreateCareProviderAccountView {
 
     private CreateCareProviderAccountPresenter presenter;
+    private EditText id;
+    private EditText firstname;
+    private EditText lastname;
+    private EditText username;
+    private EditText email;
+    private EditText phonenumber;
+    private EditText password;
+    private EditText confirmpassword;
+
+
+    private TextView create_careprovider_id_error;
+    private TextView create_carepovider_username_error;
+    private TextView create_careprovider_email_error;
+    private TextView create_carepovider_phonenumber_error;
+    private TextView create_careprovider_password_error;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +45,58 @@ public class CreateCareProviderAccountActivity extends AppCompatActivity
         setContentView(R.layout.activity_create_care_provider_account);
 
         presenter = new CreateCareProviderAccountPresenter(this, getApplicationContext());
+
+        id = findViewById(R.id.create_careprovider_id);
+        firstname = findViewById(R.id.create_careprovider_firstname);
+        lastname = findViewById(R.id.create_careprovider_lastName);
+        username= findViewById(R.id.create_careprovider_username);
+        email = findViewById(R.id.create_careprovider_email);
+        phonenumber = findViewById(R.id.create_careprovider_phone);
+        password = findViewById(R.id.create_careprovider_password);
+        confirmpassword =findViewById(R.id.create_careprovider_confirmpassword);
+
+
+        create_careprovider_id_error =findViewById(R.id.create_careprovider_id_error);
+        create_carepovider_username_error = findViewById(R.id.create_careprovider_username_error);
+        create_careprovider_email_error = findViewById(R.id.create_careprovider_email_error);
+        create_carepovider_phonenumber_error = findViewById(R.id.create_careprovider_phonenumber_error);
+        create_careprovider_password_error = findViewById(R.id.create_careprovider_password_error);
+
+
+        Button verifyButton = findViewById(R.id.create_careprovider_verify_button);
+        verifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.verifyButtonClicked(
+                        id.getText().toString(),
+                        firstname.getText().toString(),
+                        lastname.getText().toString(),
+                        username.getText().toString(),
+                        email.getText().toString(),
+                        phonenumber.getText().toString(),
+                        password.getText().toString(),
+                        confirmpassword.getText().toString()
+                );
+            }
+        });
+
+        Button cancelButton = findViewById(R.id.create_careprovider_cancel_button);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(
+                        CreateCareProviderAccountActivity.this,
+                        LoginCareProviderActivity.class
+                ));
+            }
+        });
     }
+
+
+
+
+
+
 
     @Override
     public void onStart() {
@@ -37,6 +110,7 @@ public class CreateCareProviderAccountActivity extends AppCompatActivity
      */
     @Override
     public void updateUsernameError(String message) {
+        create_carepovider_username_error.setText(message);
         // TODO: Set error message to text view
     }
 
@@ -47,6 +121,7 @@ public class CreateCareProviderAccountActivity extends AppCompatActivity
      */
     @Override
     public void updateEmailError(String message) {
+        create_careprovider_email_error.setText(message);
         // TODO: Set error message to text view
     }
 
@@ -57,6 +132,7 @@ public class CreateCareProviderAccountActivity extends AppCompatActivity
      */
     @Override
     public void updatePhoneNumberError(String message) {
+        create_carepovider_phonenumber_error.setText(message);
         // TODO: Set error message to text view
     }
 
@@ -67,6 +143,7 @@ public class CreateCareProviderAccountActivity extends AppCompatActivity
      */
     @Override
     public void updatePasswordError(String message) {
+        create_careprovider_password_error.setText(message);
         // TODO: Set error message to text view
     }
 
@@ -77,6 +154,7 @@ public class CreateCareProviderAccountActivity extends AppCompatActivity
      */
     @Override
     public void updateCareProviderIdError(String message) {
+        create_careprovider_id_error.setText(message);
         // TODO: Set error message to text view
     }
 
@@ -85,6 +163,10 @@ public class CreateCareProviderAccountActivity extends AppCompatActivity
      */
     @Override
     public void onAccountConfirmed() {
+        startActivity(new Intent(
+                CreateCareProviderAccountActivity.this,
+                CareProviderHomeActivity.class
+        ));
         finish();
     }
 }
