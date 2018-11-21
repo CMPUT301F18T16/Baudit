184603a84ad6df258cf9fdeae049ca27a947e879
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-21 10:17:01 -0700

    improving test setup/teardown

--- app/src/androidTest/java/ca/klapstein/baudit/activities/LocationActivityTest.java
+++ app/src/androidTest/java/ca/klapstein/baudit/activities/LocationActivityTest.java
@@ -1,8 +1,11 @@
 package ca.klapstein.baudit.activities;
 
 import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import ca.klapstein.baudit.data.*;
+import ca.klapstein.baudit.models.DataModel;
 import com.robotium.solo.Solo;
 import org.junit.After;
 import org.junit.Before;
@@ -15,6 +18,7 @@ import static android.support.test.InstrumentationRegistry.getInstrumentation;
 public class LocationActivityTest extends ActivityTestRule<LocationActivity> {
 
     private Solo solo;
+    private DataModel dataModel;
 
     public LocationActivityTest() {
         super(ca.klapstein.baudit.activities.LocationActivity.class);
@@ -22,12 +26,18 @@ public class LocationActivityTest extends ActivityTestRule<LocationActivity> {
 
     @Before
     public void setUp() {
+        dataModel = new DataModel(InstrumentationRegistry.getTargetContext());
+        dataModel.setOfflineLoginAccount(new Patient(
+                new Username("TESTPatient1"), new Password("foobar123"),
+                new ContactInfo(new Email("patient@example.com"), new PhoneNumber("111-111-1111"))
+        ));
         super.launchActivity(new Intent());
         solo = new Solo(getInstrumentation(), getActivity());
     }
 
     @After
     public void tearDown() {
+        dataModel.clearOfflineLoginAccount();
         solo.finishOpenedActivities();
     }
 
