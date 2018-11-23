9ce6b25daedc58f9170de1fd3b937aab24e0af3a
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-22 19:29:51 -0700

    adding todo null checks
    saaafety?

--- app/src/main/java/ca/klapstein/baudit/presenters/CareProviderHomePresenter.java
+++ app/src/main/java/ca/klapstein/baudit/presenters/CareProviderHomePresenter.java
@@ -17,31 +17,39 @@ import ca.klapstein.baudit.views.PatientRowView;
  */
 public class CareProviderHomePresenter extends HomePresenter<HomeView> {
 
-    private PatientTreeSet patientTreeSet;
     private CareProvider careProvider;
     public CareProviderHomePresenter(HomeView view, Context context) {
         super(view, context);
         careProvider = dataManager.getLoggedInCareProvider();
-        patientTreeSet = dataManager.getPatients();
     }
 
     public void onBindPatientRowViewAtPosition(PatientRowView rowView, int position) {
-        Patient patient = (Patient) patientTreeSet.toArray()[position];
-        rowView.setPatientNameText(patient.getUsername().toString());
+        if (careProvider == null || careProvider.getAssignedPatientTreeSet() == null) {
+            // TODO: error
+        } else {
+            Patient patient = (Patient) careProvider.getAssignedPatientTreeSet().toArray()[position];
+            rowView.setPatientNameText(patient.getUsername().toString());
+        }
+
     }
 
     public int getPatientCount() {
-        return patientTreeSet.size();
+        if (careProvider == null || careProvider.getAssignedPatientTreeSet() == null) {
+            // TODO: error
+            return 0;
+        } else {
+            return careProvider.getAssignedPatientTreeSet().size();
+        }
     }
 
     public void viewStarted() {
-        CareProvider account = dataManager.getLoggedInCareProvider();
-        if (account != null) {
-            view.updateUsernameDisplay(account.getUsername().toString());
-            view.updateEmailDisplay(account.getContactInfo().getEmail().toString());
-            view.updateList();
-        } else {
+        careProvider = dataManager.getLoggedInCareProvider();
+        if (careProvider == null) {
             // TODO: error
+        } else {
+            view.updateUsernameDisplay(careProvider.getUsername().toString());
+            view.updateEmailDisplay(careProvider.getContactInfo().getEmail().toString());
+            view.updateList();
         }
     }
 }
