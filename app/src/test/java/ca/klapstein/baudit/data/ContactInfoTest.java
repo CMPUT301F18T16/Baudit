795242cb2e050b0ed96b955b06a6acf437f1a685
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-22 17:55:02 -0700

    improving `ContactInfo` tests

--- app/src/test/java/ca/klapstein/baudit/data/ContactInfoTest.java
+++ app/src/test/java/ca/klapstein/baudit/data/ContactInfoTest.java
@@ -3,14 +3,22 @@ package ca.klapstein.baudit.data;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.*;
 
 public class ContactInfoTest {
+    @Mock
+    private Email mockEmail;
+    @Mock
+    private PhoneNumber mockPhoneNumber;
+
     private ContactInfo contactInfo;
 
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
         contactInfo = new ContactInfo(getExampleEmail1(), getExamplePhoneNumber1());
     }
 
@@ -61,5 +69,34 @@ public class ContactInfoTest {
         contactInfo.setEmail(getExampleEmail2());
         assertEquals(getExampleEmail2(), contactInfo.getEmail());
         assertNotEquals(getExampleEmail1(), getExampleEmail2());
+    }
+
+    @Test
+    public void testHashCodeSame() {
+        ContactInfo contactInfo1 = new ContactInfo(getExampleEmail1(), getExamplePhoneNumber1());
+        ContactInfo contactInfo2 = new ContactInfo(getExampleEmail1(), getExamplePhoneNumber1());
+        assertEquals(contactInfo1.hashCode(), contactInfo2.hashCode());
+    }
+
+    @Test
+    public void testHashCodeDifferent() {
+        ContactInfo contactInfo1 = new ContactInfo(getExampleEmail1(), getExamplePhoneNumber1());
+        ContactInfo contactInfo2 = new ContactInfo(getExampleEmail2(), getExamplePhoneNumber2());
+        assertNotEquals(contactInfo1.hashCode(), contactInfo2.hashCode());
+    }
+
+    @Test
+    public void equals() {
+        assertTrue(new ContactInfo(mockEmail, mockPhoneNumber).equals(new ContactInfo(mockEmail, mockPhoneNumber)));
+    }
+
+    @Test
+    public void neverEqualsNull() {
+        assertNotEquals(new ContactInfo(mockEmail, mockPhoneNumber), null);
+    }
+
+    @Test
+    public void equalsOtherObject() {
+        assertNotEquals(new ContactInfo(mockEmail, mockPhoneNumber), new Object());
     }
 }
