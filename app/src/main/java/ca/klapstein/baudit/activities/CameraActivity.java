3f01a4c8ecf8a5c0fed651b049229718506da51e
jalqueza <jerwynalqueza@gmail.com>
2018-11-27 01:02:26 -0700

    removed unused imports

--- app/src/main/java/ca/klapstein/baudit/activities/CameraActivity.java
+++ app/src/main/java/ca/klapstein/baudit/activities/CameraActivity.java
@@ -2,21 +2,10 @@ package ca.klapstein.baudit.activities;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.annotation.Nullable;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import ca.klapstein.baudit.R;
 
