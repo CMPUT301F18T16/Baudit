bb6141fec318d54b0ce3599eb706a706d7521fd7
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-28 15:47:58 -0700

    Adding toast message on `onLoginValidationFailure`
    adding docs

--- app/src/main/java/ca/klapstein/baudit/activities/StartActivity.java
+++ app/src/main/java/ca/klapstein/baudit/activities/StartActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import ca.klapstein.baudit.R;
 import ca.klapstein.baudit.presenters.StartPresenter;
 import ca.klapstein.baudit.views.StartView;
@@ -52,11 +53,22 @@ public class StartActivity extends AppCompatActivity implements StartView {
         super.onStart();
     }
 
+    /**
+     * On a login validation failure create a short toast message notifying
+     * the reason of the failure.
+     *
+     * @param message {@code String} message noting the reason of the failure
+     */
     @Override
     public void onLoginValidationFailure(String message) {
-        // TODO: Discuss what should happen here
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * On a login validation success proceed and start the appropriate home activity.
+     *
+     * @param homeClass
+     */
     @Override
     public void onLoginValidationSuccess(Class homeClass) {
         startActivity(new Intent(getApplicationContext(), homeClass));
