b2f303ce24f169f3f8a7783aa6858cad7fc113ea
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-28 21:31:11 -0700

    refactoring

--- app/src/main/java/ca/klapstein/baudit/views/RecordView.java
+++ app/src/main/java/ca/klapstein/baudit/views/RecordView.java
@@ -3,10 +3,6 @@ package ca.klapstein.baudit.views;
 public interface RecordView extends View {
     void updateTitleField(String title);
     void updateCommentField(String comment);
-
-    void commitRecord();
-
     void commitRecordFailure();
-
     void commitRecordSuccess();
 }
