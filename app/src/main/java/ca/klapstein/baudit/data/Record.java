781f6eb32adcf4fb6220de4d615aeb9ef9a30f4d
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-30 16:06:38 -0700

    adding list of `RecordPhoto`s to `Record`

--- app/src/main/java/ca/klapstein/baudit/data/Record.java
+++ app/src/main/java/ca/klapstein/baudit/data/Record.java
@@ -25,6 +25,7 @@ public class Record implements Comparable<Record> {
     private GeoLocation geoLocation;
     private ArrayList<BodyPhotoCoords> bodyPhotoCoords = new ArrayList<>();
     private ArrayList<String> keywords = new ArrayList<>();
+    private ArrayList<RecordPhoto> photos = new ArrayList<RecordPhoto>();
     private UUID recordId;
 
     public Record() {
@@ -212,5 +213,13 @@ public class Record implements Comparable<Record> {
 
     private void setRecordId(UUID recordId) {
         this.recordId = recordId;
+    }
+
+    public ArrayList<RecordPhoto> getPhotos() {
+        return this.photos;
+    }
+
+    public void addPhoto(RecordPhoto photo) {
+        this.photos.add(photo);
     }
 }
