8f60c792703f45935da246a24bcf749fb7280d2d
Nathan Klapstein <nklapste@ualberta.ca>
2018-12-03 12:15:58 -0700

    adding javadoc

--- app/src/main/java/ca/klapstein/baudit/data/RecordPhoto.java
+++ app/src/main/java/ca/klapstein/baudit/data/RecordPhoto.java
@@ -8,20 +8,33 @@ import ca.klapstein.baudit.util.BitmapEncoderUtil;
 public class RecordPhoto {
     private static final int MAX_PHOTO_BYTES = 65535;
 
+    /**
+     * Bitmap is saved as a base64 string for easy saving in local and remote storage.
+     */
     private String bitmapString;
 
     public RecordPhoto(@NonNull Bitmap bitmap) {
         setPhoto(bitmap);
     }
 
+    /**
+     * Decode the {@code bitmapString} and return its representing {@code Bitmap} for usage.
+     *
+     * @return {@code Bitmap}
+     */
     public Bitmap getBitmap() {
         return BitmapEncoderUtil.decodeBase64(bitmapString);
     }
 
+    /**
+     * Encode a given {@code Bitmap} into a base64 string and save it within the {@code bitmapString} field.
+     *
+     * @param bitmap {@code Bitmap}
+     */
     public void setPhoto(@NonNull Bitmap bitmap) {
         if (bitmap.getByteCount() > MAX_PHOTO_BYTES) {
             bitmapString = BitmapEncoderUtil.encodeTobase64(
-                ThumbnailUtils.extractThumbnail(bitmap, 255, 255)
+                    ThumbnailUtils.extractThumbnail(bitmap, 255, 255)
             );
         } else {
             bitmapString = BitmapEncoderUtil.encodeTobase64(bitmap);
