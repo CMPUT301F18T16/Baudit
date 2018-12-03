c4661c45df8f11e98d0e8ab2f3536660c55c6a32
Nathan Klapstein <nklapste@ualberta.ca>
2018-12-03 12:40:51 -0700

    adding basic filtering/searching

--- app/src/main/java/ca/klapstein/baudit/presenters/CareProviderProblemListPresenter.java
+++ app/src/main/java/ca/klapstein/baudit/presenters/CareProviderProblemListPresenter.java
@@ -3,13 +3,16 @@ package ca.klapstein.baudit.presenters;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.util.Log;
-
 import ca.klapstein.baudit.data.Patient;
 import ca.klapstein.baudit.data.Problem;
 import ca.klapstein.baudit.data.ProblemTreeSet;
 import ca.klapstein.baudit.data.Username;
 import ca.klapstein.baudit.views.CareProviderProblemListView;
 import ca.klapstein.baudit.views.ProblemRowView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class CareProviderProblemListPresenter extends Presenter<CareProviderProblemListView> {
 
@@ -54,5 +57,25 @@ public class CareProviderProblemListPresenter extends Presenter<CareProviderProb
             Log.e(TAG, "failed to get problem data", e);
             view.updateAccountLoadError();
         }
+    }
+
+    public void filterProblemsByKeyWords(CharSequence constraint) {
+        try {
+            patient = dataManager.getPatient(patient.getUsername());
+            problemTreeSet.clear();
+            problemTreeSet.addAll(patient.getProblemTreeSet());
+        } catch (Exception e) {
+            Log.e(TAG, "failed to obtain patient account info", e);
+        }
+
+        ArrayList<String> searchTokens = new ArrayList<>(Arrays.asList(constraint.toString().toLowerCase().split(" ")));
+        Log.d(TAG, "filtering with tokens: " + searchTokens);
+        patient = dataManager.getLoggedInPatient();
+        Problem[] problemArray = problemTreeSet.toArray(new Problem[0]);
+        for (Problem aProblemArray : problemArray) {
+            if (Collections.disjoint(searchTokens, aProblemArray.getKeywords()))
+                problemTreeSet.remove(aProblemArray);
+        }
+        view.updateList();
     }
 }
