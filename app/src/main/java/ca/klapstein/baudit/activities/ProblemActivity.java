d37980548fd86fafaf568aa25f8265be4f06530c
dcones <dcones@ualberta.ca>
2018-12-02 22:46:37 -0700

    Remove commented blocks

--- app/src/main/java/ca/klapstein/baudit/activities/ProblemActivity.java
+++ app/src/main/java/ca/klapstein/baudit/activities/ProblemActivity.java
@@ -14,13 +14,26 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import ca.klapstein.baudit.R;
 import ca.klapstein.baudit.data.Record;
 import ca.klapstein.baudit.data.RecordTreeSet;
@@ -28,11 +41,6 @@ import ca.klapstein.baudit.fragments.DatePickerDialogFragment;
 import ca.klapstein.baudit.fragments.TimePickerDialogFragment;
 import ca.klapstein.baudit.presenters.ProblemPresenter;
 import ca.klapstein.baudit.views.ProblemView;
-
-import java.text.DateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 
 import static ca.klapstein.baudit.activities.MapRecordsActivity.MAP_RECORDS_MODE;
 import static ca.klapstein.baudit.activities.MapRecordsActivity.MAP_RECORDS_PROBLEM_POSITION;
@@ -175,7 +183,6 @@ public class ProblemActivity extends AppCompatActivity
     @Override
     public void onStart() {
         super.onStart();
-//        presenter.viewStarted(problemPosition);
         updateRecordCountText();
         updateTimeButton(problemTime.getTime());
         updateDateButton(problemTime.getTime());
