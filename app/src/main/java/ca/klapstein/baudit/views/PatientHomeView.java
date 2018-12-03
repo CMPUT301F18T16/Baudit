ddf4795d40aa2ec7af45964086d0835e2416d8df
Nathan Klapstein <nklapste@ualberta.ca>
2018-12-03 13:45:48 -0700

    refactoring

--- app/src/main/java/ca/klapstein/baudit/views/PatientHomeView.java
+++ app/src/main/java/ca/klapstein/baudit/views/PatientHomeView.java
@@ -2,11 +2,8 @@ package ca.klapstein.baudit.views;
 
 public interface PatientHomeView extends ProblemListView {
     void updateDeleteProblemError();
-
     void updateUsernameDisplay(String username);
-
     void updateEmailDisplay(String email);
-
     void updateAccountLoadError();
     void updateProblemNumber(int problemNumber);
 }
