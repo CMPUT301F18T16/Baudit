8836ecaa1ac899db0f4b1ad9d5d0a19a087cc546
Nathan Klapstein <nklapste@ualberta.ca>
2018-12-02 17:06:15 -0700

    adding a bitmap to base64 string encoder

---
+++ app/src/main/java/ca/klapstein/baudit/util/BitmapEncoder.java
@@ -0,0 +1,25 @@
+package ca.klapstein.baudit.util;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+public class BitmapEncoder {
+    @NonNull
+    public static String encodeTobase64(@NonNull Bitmap image) {
+        Bitmap immagex = image;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        immagex.compress(Bitmap.CompressFormat.PNG, 90, baos);
+        byte[] b = baos.toByteArray();
+        String imageEncoded = Base64.encodeToString(b, Base64.DEFAULT);
+        return imageEncoded;
+    }
+
+    public static Bitmap decodeBase64(String input) {
+        byte[] decodedByte = Base64.decode(input, 0);
+        return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
+    }
+}
