180e837a1b1c714ac9db3f600a789da0c037f1ec
Nathan Klapstein <nklapste@ualberta.ca>
2018-12-02 19:43:08 -0700

    annotations

--- app/src/main/java/ca/klapstein/baudit/data/BodyLocationPhoto.java
+++ app/src/main/java/ca/klapstein/baudit/data/BodyLocationPhoto.java
@@ -1,30 +1,36 @@
 package ca.klapstein.baudit.data;
 
 import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import org.jetbrains.annotations.NotNull;
 
 public class BodyLocationPhoto {
 
+    @NonNull
     private Bitmap photo;
+    @NonNull
     private String label;
 
-    public BodyLocationPhoto(Bitmap photo, String label) {
+    public BodyLocationPhoto(@NonNull Bitmap photo, @NonNull String label) {
         setPhoto(photo);
         this.label = label;
     }
 
+    @NotNull
     public Bitmap getPhoto() {
         return photo;
     }
 
-    public void setPhoto(Bitmap photo) {
+    public void setPhoto(@NotNull Bitmap photo) {
         this.photo = photo;
     }
 
+    @NotNull
     public String getLabel() {
         return label;
     }
 
-    public void setLabel(String label) {
+    public void setLabel(@NotNull String label) {
         this.label = label;
     }
 }
