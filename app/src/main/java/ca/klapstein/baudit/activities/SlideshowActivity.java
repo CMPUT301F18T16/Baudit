fb97b145364fbe421eed0aa47dda408ed9a525f5
Nathan Klapstein <nklapste@ualberta.ca>
2018-12-03 10:36:43 -0700

    refactoring

--- app/src/main/java/ca/klapstein/baudit/activities/SlideshowActivity.java
+++ app/src/main/java/ca/klapstein/baudit/activities/SlideshowActivity.java
@@ -75,15 +75,19 @@ public class SlideshowActivity extends AppCompatActivity implements SlideShowVie
             this.bitmapArrayList.addAll(bitmapArrayList);
         }
 
+        /**
+         * Get the number of photos stored in bitmap array.
+         *
+         * @return {@code int}
+         */
         @Override
         public int getCount() {
-            //TODO: Get length of the photos stored in bitmap Array
-            // return image_resource.length; TEST
             return bitmapArrayList.size();
         }
 
         /**
-         * Determines whether a page View is associated with a specific key object as returned by instantiateItem(ViewGroup, int).
+         * Determines whether a page View is associated with a specific key object as returned by
+         * {@code instantiateItem(ViewGroup, int)}.
          */
         @Override
         public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
@@ -91,23 +95,19 @@ public class SlideshowActivity extends AppCompatActivity implements SlideShowVie
         }
 
         /**
-         * Create the page for the given position
+         * Create the slideshow page for the given image.
          */
         @NonNull
         @Override
         public Object instantiateItem(@NonNull ViewGroup container, int position) {
-
             layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View item_view = layoutInflater.inflate(R.layout.slideshow_layout, container, false);
+            View slideShowLayout = layoutInflater.inflate(R.layout.slideshow_layout, container, false);
 
-            ImageView imageView = item_view.findViewById(R.id.image_view);
-            // imageView.setImageResource(image_resource[position]);  //TEST CASE
-
-            //TODO:update empty bitmapArray & setImageResource to the images on the list
+            ImageView imageView = slideShowLayout.findViewById(R.id.image_view);
             imageView.setImageBitmap(RotateBitmap90(bitmapArrayList.get(position)));
 
-            container.addView(item_view);
-            return item_view;
+            container.addView(slideShowLayout);
+            return slideShowLayout;
         }
 
         /**
