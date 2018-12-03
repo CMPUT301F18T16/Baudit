6969f2c4292338ff72c0b6787073987ed1b02383
Nathan Klapstein <nklapste@ualberta.ca>
2018-12-03 13:50:12 -0700

    refactoring removing `HomeView`

--- app/src/main/java/ca/klapstein/baudit/views/CareProviderHomeView.java
+++ app/src/main/java/ca/klapstein/baudit/views/CareProviderHomeView.java
@@ -1,11 +1,16 @@
 package ca.klapstein.baudit.views;
 
-public interface CareProviderHomeView extends HomeView {
+public interface CareProviderHomeView extends View {
     void startScanQRCode();
-
     void updateScanQRCodeError();
-
     void updatePatientCount(int patientNumber);
-
     void updateRemovePatientError();
+
+    void updateUsernameDisplay(String username);
+
+    void updateEmailDisplay(String email);
+
+    void updateAccountLoadError();
+
+    void updateList();
 }
