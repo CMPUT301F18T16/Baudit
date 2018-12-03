d371062a4d3dd935b1b089bf70c099f9b3f7c01f
Nathan Klapstein <nklapste@ualberta.ca>
2018-12-03 13:56:52 -0700

    removing unused views

--- app/src/main/java/ca/klapstein/baudit/views/PhotoView.java
+++
@@ -1,9 +0,0 @@
-package ca.klapstein.baudit.views;
-
-import android.graphics.Bitmap;
-
-public interface PhotoView extends View {
-    void updatePhotoImage(Bitmap bitmap);
-
-    void updatePhotoError();
-}
