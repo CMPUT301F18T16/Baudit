f58e67426ce7fc00f3e0e99fb76222d74f9358e8
DeveloperJeffb <33044463+DeveloperJeffb@users.noreply.github.com>
2018-12-02 19:01:58 -0700

    adding Slideshow branch with Uploadbodyphoto

---
+++ app/src/main/java/ca/klapstein/baudit/activities/SlideshowActivity.java
@@ -0,0 +1,15 @@
+package ca.klapstein.baudit.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.klapstein.baudit.R;
+
+public class SlideshowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_slideshow );
+    }
+}
